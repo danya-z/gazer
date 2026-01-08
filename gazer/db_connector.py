1182ac6b8f4ead20c3dbef25a6f6b2b52057dea3
danya-z <danilazhum@gmail.com>
2026-01-08 01:37:07 -0800

    Added connector class to handle fundamental db operations

---
+++ gazer/db_connector.py
@@ -0,0 +1,32 @@
+import psycopg2
+import pandas as pd
+
+class DBConnector:
+  def __init__(self, host, port, database, user, password):
+    self.host = host
+    self.port = port
+    self.database = database
+    self.user = user
+    self.password = password
+    self.conn = None
+
+    def connect(self):
+      self.conn = psycopg2.connect(
+        host=self.host,
+        port=self.port,
+        database=self.database,
+        user=self.user,
+        password=self.password
+      )
+
+    def execute_query(self, sql):
+      cur = self.conn.cursor()
+      cur.execute(sql)
+      results = cur.fetchall()
+      columns = [desc[0] for desc in cur.description]
+      cur.close()
+      return pd.DataFrame(results, columns=columns)
+
+    def close(self):
+      if self.conn:
+        self.conn.close()
