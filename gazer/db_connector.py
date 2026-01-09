ff00a95c34ebd8e0db6ad1d5b6d2f33c1410b3bb
danya-z <danilazhum@gmail.com>
2026-01-09 14:42:45 -0800

    Fixed indentation issues

--- gazer/db_connector.py
+++ gazer/db_connector.py
@@ -10,30 +10,30 @@ class DBConnector:
     self.password = password
     self.conn = None
 
-    def connect(self):
-      self.conn = psycopg2.connect(
-        host=self.host,
-        port=self.port,
-        database=self.database,
-        user=self.user,
-        password=self.password
-      )
+  def connect(self):
+    self.conn = psycopg2.connect(
+      host=self.host,
+      port=self.port,
+      database=self.database,
+      user=self.user,
+      password=self.password
+    )
 
-    def execute_query(self, sql):
-      cur = self.conn.cursor()
-      cur.execute(sql)
-      results = cur.fetchall()
-      columns = [desc[0] for desc in cur.description]
-      cur.close()
-      return pd.DataFrame(results, columns=columns)
+  def execute_query(self, sql):
+    cur = self.conn.cursor()
+    cur.execute(sql)
+    results = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    cur.close()
+    return pd.DataFrame(results, columns=columns)
 
-    def execute_raw(self, sql):
-      cur = self.conn.cursor()
-      cur.execute(sql)
-      results = cur.fetchall()
-      cur.close()
-      return results
+  def execute_raw(self, sql):
+    cur = self.conn.cursor()
+    cur.execute(sql)
+    results = cur.fetchall()
+    cur.close()
+    return results
 
-    def close(self):
-      if self.conn:
-        self.conn.close()
+  def close(self):
+    if self.conn:
+      self.conn.close()
