8c25fbcbec19d3b781c66ed257d15fc6d5076967
danya-z <danilazhum@gmail.com>
2026-02-09 17:43:31 -0800

    Adding a separate standard widget for all error messages

---
+++ gazer/error_display.py
@@ -0,0 +1,7 @@
+# error_display.py
+# Centralized error display widget for Gazer.
+# Any screen can push this to show errors with copy-to-clipboard support.
+# TODO: Implement reusable ErrorScreen (extract from tui_main.py)
+#   - Non-fatal errors: dismiss and return to previous screen
+#   - Fatal errors: dismiss and quit
+#   - Copy error details to clipboard
